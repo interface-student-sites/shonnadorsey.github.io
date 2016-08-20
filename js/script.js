5d705d07081dacf7610bb319dffcd692d393e1c7
shonnadorsey <shonna@interfaceschool.com>
2016-08-20 11:40:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-$(document).ready( function() {
-   
-   //if($(window).width() > 615) {
-	   var topOfOthDiv = $("#pageTop").offset().top;
-	   $(window).scroll(function() {
-		   if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-			   $("#nav-brand").fadeIn(500); //reached the desired point -- show div
-		   }
-			else {
-				$("#nav-brand").fadeOut(500);
-			}
-			
-	   });
-   //}   
-    
+$(document).ready(function(){
+	$('button').click(function(){
+		$('img').fadeOut('slow');	
+	});
+	$('#button-two').click(function(){
+		$('img').fadeIn('slow');	
+	});
+	
 });
-
-
-  
